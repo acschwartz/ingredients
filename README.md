edfcaf3453674b6ca7bd45eae703af7a22f8d3b2
Anna Schwartz <schw1781@umn.edu>
2017-12-02 21:07:49 -0600

    Ingredients now data records referenced in products.ingredients

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 # database decisons
 products: unique index on pName (pName must be unique now)
+ingredients: _id is ingredient name (may want to do same for product?)
+
+# Challenges
+## Get list of ingredient _id's into products document
+
+Input: List of ingredient names
+If item not in ingredients collection, add it.
+
+First tried for loop that iterates through list of ingredient names, uses findAndModify to find or insert that ingredient, which returns that ingredient's _id.
+
+Problem: asynch.
+
+Attempted solution: callback hell.
+
+New solution: in ingredients collection, use _id for document name. Now we do not have to fetch the id; we already have it from the start.
+Use IIFE ([Immediately Invoked Function Expression](http://javascriptissexy.com/understand-javascript-closures-with-ease/)) to handle asynch problem in for loop.
--- routes/api.js
+++ routes/api.js
@@ -2,45 +2,49 @@ var express = require('express');
 var router = express.Router();
 var ObjectId = require('mongodb').ObjectID;
 
-// function insertProduct(req){
-//
-// }
 
 // insert api
 router.post('/insert', function(req, res, next) {
 
-  // check that fields are not null done on client side in script.js
-  // TODO: handle error if product already in db
-    // don't need to check bc unique index prevents insert... simply need to handle error
-    // display error message on client side
-  // TODO: ingredients as data objects
+  // If ingredient not in db, insert it
+  var ingredients = req.body.ingredients.replace(/\.|\?|!/g, '').split(',');  //may need more work on regex
+  for (i = 0; i < ingredients.length; i++) {
+    ingredients[i] = function(j) {  // the j parametric variable is passed in on invocation of this IIFE
+      var ing = ingredients[j].trim().toLowerCase();
+      req.db.collection('ingredients').findAndModify(
+        { _id: ing }, //query
+        [], //sort
+        { $setOnInsert: { _id: ing } }, // update
+        { new: true,   // return new doc if one is upserted
+        upsert: true});
+      return ing;
+    }(i);
+  }
 
-
-    // Insert the new product to the database
-    // using callback
-    var ingredientList = req.body.ingredients.replace(/\.|\?|!/g, '').split(',');  //may need more work on regex
-    for (i = 0; i < ingredientList.length; i++){
-      ingredientList[i] = ingredientList[i].trim().toLowerCase();
-    }
-
-    req.db.collection('products').insertOne({ 'pName': req.body.pName, 'ingredients': ingredientList }, function(err, results){
-      if (err){
-        console.log("error")
-      } else{
-        console.log("insert complete")
-        //send success status to client side
-        res.status(200).send('success');
-      }
+  (function insertProduct(){
+    // Insert the new product to the database using callback
+    req.db.collection('products').insertOne({ '_id': req.body.pName, 'ingredients': ingredients },
+      function(err, results){
+        if (err){
+          console.log("insert product error")
+        } else{
+          console.log("insert complete")
+          //send success status to client side
+          res.status(200).send('success');
+        }
     });
 
     console.log("async"); // see when this prints - could print before insert is done
-    // which is why we want to put "insert done"/'succes' in the callback, and not below
+    // which is why we want to put "insert done"/'success' in the callback, and not below
 
     //send success status to client side
     // res.status(200).send('success');
+  })();
+
 });
 
-// delete api
+
+// delete api for TODO list/template
 router.post('/delete', function(req, res, next) {
     // Delete the task sent specified the client side
     // using promise
@@ -57,7 +61,7 @@ router.post('/delete', function(req, res, next) {
     // res.status(200).send('success');
 });
 
-// update api
+// update api for TODO list/template
 router.post('/update', function(req, res, next) {
     // Update the status of the task to done
     req.db.collection('schw1781_todo').updateOne({ _id: ObjectId(req.body.id) }, { $set: { "done": req.body.status == 1 } });
